855ae249487769368479fa646631a8186837909e
Kardi <kgland06@gmail.com>
2024-10-01 14:21:09 +0700

    classnames

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Button from "./Button";
-import PropTypes from "prop-types";
+
 const App = () => {
     return (
         <div>
@@ -11,18 +11,4 @@ const App = () => {
     )
 }
 
-Button.PropTypes = {
-    checkVariationValue: ({primary, secondary, success, warning, danger})=>{
-        const count = Number(!!primary)
-            + Number(!!secondary)
-            + Number(!!success) 
-            + Number(!!warning)
-            + Number(!!danger);
-        
-        if(count > 1){
-            throw new Error("Only one of primary, secondary, success, warning, danger can be true");
-        }
-        
-    }
-};
 export default App;
--- src/Button.js
+++ src/Button.js
@@ -1,3 +1,6 @@
+// import PropTypes from "prop-types";
+import classnames from "classnames";
+
 const Button = ({
     children,
     primary,
@@ -8,9 +11,30 @@ const Button = ({
     rounded,
     outline,
 }) => {
+    const classes =  classnames('px-3 py-1.5 border', {
+        'border-blue-600 bg-blue-500 text-white': primary,
+        'border-gray-900 bg-gray-900 text-white': secondary,
+        'border-green-600 bg-green-500 text-white': success,
+        'border-yellow-400 bg-yellow-400 text-white': warning,
+        'border-red-600 bg-red-600 text-white': danger,
+    });
     return (
-        <button>{children}</button>
+        <button className={classes}>{children}</button>
     );
 }
+Button.propTypes = {
+    checkVariationValue: ({primary, secondary, success, warning, danger})=>{
+        const count = Number(!!primary)
+            + Number(!!secondary)
+            + Number(!!success) 
+            + Number(!!warning)
+            + Number(!!danger);
+        
+        if(count > 1){
+            throw new Error("Only one of primary, secondary, success, warning, danger can be true");
+        }
+        
+    }
+};
 
 export default Button;
