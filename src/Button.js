2c5b4062bcf098b533f91c09d880d3a29b0fe82d
Kardi <kgland06@gmail.com>
2024-10-18 14:55:27 +0700

    button twmerge

--- src/Button.js
+++ src/Button.js
@@ -1,5 +1,6 @@
 // import PropTypes from "prop-types";
 import classnames from "classnames";
+import { twMerge } from "tailwind-merge";
 
 const Button = ({
     children,
@@ -11,13 +12,20 @@ const Button = ({
     rounded,
     outline,
 }) => {
-    const classes =  classnames('px-3 py-1.5 border', {
+    const classes =  twMerge(classnames('px-3 py-1.5 border', {
         'border-blue-600 bg-blue-500 text-white': primary,
         'border-gray-900 bg-gray-900 text-white': secondary,
         'border-green-600 bg-green-500 text-white': success,
         'border-yellow-400 bg-yellow-400 text-white': warning,
         'border-red-600 bg-red-600 text-white': danger,
-    });
+        'rounded-full': rounded,
+        'bg-white': outline,
+        'text-blue-500': outline && primary,
+        'text-gray-900': outline && secondary,
+        'text-green-500': outline && success,
+        'text-yellow-500': outline && warning,
+        'text-red-500': outline && danger,
+    }));
     return (
         <button className={classes}>{children}</button>
     );
