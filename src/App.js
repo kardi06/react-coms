cc59c17bcb984cef743c95c38e27b55ba68a9f0c
Kardi <kgland06@gmail.com>
2024-11-05 06:34:09 +0700

    passing props through

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import {GoBell, GoCloudDownload, GoDatabase  } from "react-icons/go";
 import Button from "./Button";
 
 const App = () => {
+    const handleClick = () => {
+        console.log('Button Clicked!');
+    }
     return (
         <div>
-            <div><Button success rounded outline><GoBell/>Click Me!</Button></div>
-            <div><Button primary rounded><GoCloudDownload /> Now</Button></div>
+            <div><Button success rounded outline onClick={handleClick}><GoBell/>Click Me!</Button></div>
+            <div><Button primary rounded onMouseEnter={handleClick}><GoCloudDownload /> Now</Button></div>
             <div><Button secondary><GoDatabase/>Seal the Deal</Button></div>
             <div><Button danger outline>Close Ads</Button></div>
         </div>
--- src/Button.js
+++ src/Button.js
@@ -11,6 +11,7 @@ const Button = ({
     danger,
     rounded,
     outline,
+    ...rest
 }) => {
     const classes =  twMerge(classnames('flex items-center px-3 py-1.5 border', {
         'border-blue-600 bg-blue-500 text-white': primary,
@@ -27,7 +28,7 @@ const Button = ({
         'text-red-500': outline && danger,
     }));
     return (
-        <button className={classes}>{children}</button>
+        <button {...rest} className={classes}>{children}</button>
     );
 }
 Button.propTypes = {
