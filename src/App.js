278c496b4858e719170ee72d9761b3f70c62fc23
Kardi <kgland06@gmail.com>
2024-11-04 21:06:56 +0700

    event handler

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
+import {GoBell, GoCloudDownload, GoDatabase  } from "react-icons/go";
 import Button from "./Button";
 
 const App = () => {
     return (
         <div>
-            <div><Button success rounded outline>Click Me!</Button></div>
-            <div><Button primary rounded>Buy Now</Button></div>
-            <div><Button secondary>Seal the Deal</Button></div>
+            <div><Button success rounded outline><GoBell/>Click Me!</Button></div>
+            <div><Button primary rounded><GoCloudDownload /> Now</Button></div>
+            <div><Button secondary><GoDatabase/>Seal the Deal</Button></div>
             <div><Button danger outline>Close Ads</Button></div>
         </div>
     )
--- src/Button.js
+++ src/Button.js
@@ -12,7 +12,7 @@ const Button = ({
     rounded,
     outline,
 }) => {
-    const classes =  twMerge(classnames('px-3 py-1.5 border', {
+    const classes =  twMerge(classnames('flex items-center px-3 py-1.5 border', {
         'border-blue-600 bg-blue-500 text-white': primary,
         'border-gray-900 bg-gray-900 text-white': secondary,
         'border-green-600 bg-green-500 text-white': success,
