cf2af8efcdfbb11783f4dccfdbfc45e1f9de7e12
Kardi <kgland06@gmail.com>
2024-12-13 22:13:19 +0700

    dropdown components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import Accordion from "./components/Accordion";
+import Dropdown from "./components/Dropdown";
 
-const App = () => {
-    const items = [
-        {   
-            id: 'ckkc25',
-            label: 'Can I use React on a project?',
-            content: 'You can use React on any project you like. We like to use it on projects with large-scale UIs.'
-        },
-        {
-            id: 'ckkc2502',
-            label: 'Can I use javascript on a project?',
-            content: 'You can use javascript on any project you like. We like to use it on projects with large-scale UIs.'
-        },
-        {
-            id: 'ckkc25020420',
-            label: 'Can I use CSS on a project?',
-            content: 'You can use CSS on any project you like. We like to use it on projects with large-scale UIs.'
-        }
-    ]
-    return (
-        <Accordion items={items}/>
-    )
+function App() {
+    const options = [
+        {label: 'Red', value: 'red'},
+        {label: 'Green', value: 'green'},
+        {label: 'Blue', value: 'blue'},
+    ];
+
+    return <Dropdown options={options}/>;
 }
 
 export default App;
---
+++ src/pages/AccordionPage.js
@@ -0,0 +1,26 @@
+import Accordion from "../components/Accordion";
+
+const AccordionPage = () => {
+    const items = [
+        {   
+            id: 'ckkc25',
+            label: 'Can I use React on a project?',
+            content: 'You can use React on any project you like. We like to use it on projects with large-scale UIs.'
+        },
+        {
+            id: 'ckkc2502',
+            label: 'Can I use javascript on a project?',
+            content: 'You can use javascript on any project you like. We like to use it on projects with large-scale UIs.'
+        },
+        {
+            id: 'ckkc25020420',
+            label: 'Can I use CSS on a project?',
+            content: 'You can use CSS on any project you like. We like to use it on projects with large-scale UIs.'
+        }
+    ]
+    return (
+        <Accordion items={items}/>
+    )
+}
+
+export default AccordionPage;
