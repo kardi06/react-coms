b8babb2b833a95bca0f870a0524ea8a5347bc073
Kardi <kgland06@gmail.com>
2024-12-16 07:33:31 +0700

    label selection

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -19,14 +19,10 @@ const Dropdown = ({options, selection, onSelect}) => {
         )
     });
     
-    let content = 'Select...';
-    
-    if(selection) {
-        content = selection.label
-    }
+   
     return (
         <div>
-            <div onClick={handleClick}>{content}</div>
+            <div onClick={handleClick}>{selection?.label || 'Select...'}</div>
             {isOpen &&<div>{renderedOptions}</div>}
         </div>
     )
