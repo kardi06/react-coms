a64ba3460049b607feeb0d3ef5a35fbacd6b3f01
Kardi <kgland06@gmail.com>
2025-02-12 07:22:58 +0700

    basic show modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,6 +1,9 @@
 const Modal = () => {
     return(
-        <div>Modal</div>
+        <div>
+            <div className="absolute inset-0 bg-gray-300 opacty-80"></div>
+            <div className="absolute inset-40 p-10 bg-white">Aku Modal</div>
+        </div>
     )
 }
 
