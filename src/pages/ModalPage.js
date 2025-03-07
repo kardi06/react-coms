c5c1f8ff35f30fed6b38e5c7426ae835153438c2
Kardi <kgland06@gmail.com>
2025-03-07 23:51:51 +0700

    close Modal

--- src/pages/ModalPage.js
+++ src/pages/ModalPage.js
@@ -8,10 +8,14 @@ const ModalPage = () => {
         setShowModal(true);
     }
 
+    const handleClose = () => {
+        setShowModal(false)
+    }
+
     return (
         <div className="relative">
             <Button onClick={handleClick} primary>Open Modal</Button>
-            {showModal && <Modal/>}
+            {showModal && <Modal onClose={handleClose}/>}
         </div>
     )
 }
