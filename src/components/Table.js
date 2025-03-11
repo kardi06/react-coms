3bd5458078289fb4c2f21b5d85ccbc11a8606cf7
Kardi <kgland06@gmail.com>
2025-03-11 07:19:47 +0700

    component table html setup

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,8 +1,17 @@
 const Table = ({data}) => {
     return (
-        <div>
-            {data.length}
-        </div>
+        <table>
+        <thead>
+            <tr>
+            <th>Fruit</th>
+            <th>Color</th>
+            <th>Score</th>
+            </tr>
+        </thead>
+        <tbody>
+            
+        </tbody>
+    </table>
     )
 }
 
