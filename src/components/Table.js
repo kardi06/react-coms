169e8b0690d28ed69a2ccd6d8e058521e87eadc9
Kardi <kgland06@gmail.com>
2025-03-17 22:29:00 +0700

    nested map

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,13 +3,12 @@ const Table = ({data, config}) => {
         return <th key={column.label}>{column.label}</th>
     })
     const rendereRows = data.map((fruit) => {
+        const renderedCells = config.map((column) => {
+            return <td className="p-2" key={column.label}>{column.render(fruit)}</td>
+        })
         return (
             <tr key={fruit.name} className="border-b">
-                <td className="p-3">{config[0].render(fruit)}</td>
-                <td className="p-3">
-                    <div className={`p-3 m-2 ${config[1].render(fruit)}`}></div>
-                </td>
-                <td className="p-3">{config[2].render(fruit)}</td>
+                {renderedCells}
             </tr>
         )
     })
--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+// import { render } from "@testing-library/react";
 import Table from "../components/Table";
 
 const TablePage = () => {
